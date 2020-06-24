468f69170b8132ee972024c14cb1cd9dd0d94cf5
nasrin-moshayedi <n.mosh4yedi@gmail.com>
2020-06-24 12:10:36 +0430

    redux persist add

--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -2,19 +2,30 @@ import thunk from "redux-thunk";
 import {createLogger} from "redux-logger";
 import {applyMiddleware, compose, createStore} from "redux";
 import allReducers from "../Reducer";
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+    key: "root",
+    storage,
+    // whitelist: ["Reducer"] // which reducer want to store
+};
 
 const middleware = [thunk];
 // if (process.env.NODE_ENV === "development") {
 middleware.push(createLogger());
 // }
 
+const pReducer = persistReducer(persistConfig, allReducers);
 
 // default middleware
 const Store = createStore(
-    allReducers,
+    // allReducers,
+    pReducer,
+
     composeEnhancers(
         applyMiddleware(...middleware)
     )
@@ -22,3 +33,4 @@ const Store = createStore(
 
 export default Store;
 
+export const persistor = persistStore(Store);
--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "../src/Styles/bootstrap.min.css";
 import '../src/Styles/style.css';
 import {BrowserRouter} from "react-router-dom";
-import Store from "./Redux/Store/Store";
+import Store, {persistor}  from "./Redux/Store/Store";
+import {PersistGate} from "redux-persist/lib/integration/react";
 import { Provider } from "react-redux";
 
 
 ReactDOM.render(
     <Provider store={Store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <PersistGate persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
     </Provider>
     , document.getElementById('root'));
 
